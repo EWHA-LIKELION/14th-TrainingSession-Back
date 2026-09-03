c38db8bfc40fd20220447f69ab580c6a8dcab472
bomee1123 <bomee1123@ewha.ac.kr>
2026-09-03 20:48:36 +0900

    fix: settings storages

--- config/settings.py
+++ config/settings.py
@@ -91,31 +91,14 @@ MIDDLEWARE = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-AWS_STORAGE_BUCKET_NAME = env('AWS_STORAGE_BUCKET_NAME', default='')
-if AWS_STORAGE_BUCKET_NAME:
-    INSTALLED_APPS += ['storages']
-    AWS_S3_REGION_NAME = env('AWS_S3_REGION_NAME', 
-default='ap-northeast-2')
-    AWS_S3_CUSTOM_DOMAIN = env('AWS_S3_CUSTOM_DOMAIN')
-    AWS_DEFAULT_ACL = None
-    AWS_QUERYSTRING_AUTH = False
-    AWS_S3_FILE_OVERWRITE = False
-    STORAGES = {
-        'default': {
-            'BACKEND': 'storages.backends.s3.S3Storage',
-            'OPTIONS': {
-                'bucket_name': AWS_STORAGE_BUCKET_NAME,
-                'region_name': AWS_S3_REGION_NAME,
-                'custom_domain': AWS_S3_CUSTOM_DOMAIN,
-                'file_overwrite': False,
-                'querystring_auth': False,
-            },
-        },
-        'staticfiles': {
-            'BACKEND': 
-'whitenoise.storage.CompressedManifestStaticFilesStorage',
-        },
-    }
+STORAGES = {
+    'default': {
+        'BACKEND': 'django.core.files.storage.FileSystemStorage',
+    },
+    'staticfiles': {
+        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+    },
+}
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True # 쿠키가 cross-site HTTP 요청에 포함됨
