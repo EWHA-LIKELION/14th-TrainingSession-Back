5f407fd2621550ddda3c66cb8a0088106f83bd80
Yeonwoo Lee <yeonwoo845@gmail.com>
2026-09-08 20:12:20 +0900

    feat:settings.py

--- config/settings.py
+++ config/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
-CSRF_TRUSTED_ORIGINS = env.list('CSRF_TRUSTED_ORIGINS', "").split(",")
+CSRF_TRUSTED_ORIGINS = env.str('CSRF_TRUSTED_ORIGINS', "").split(",")
 DATABASES = {
     'default': env.db(default="sqlite:///db.sqlite3")
 }
